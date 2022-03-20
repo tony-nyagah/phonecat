66e3e6d9f44ac26554ff1626f0f4667e89d36116
Antony Nyagah <tony.m.nyagah@gmail.com>
2022-03-20 11:01:50 +0300

    initial setup for the project

---
+++ vue-phonecat/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />-->
+  <p>Nothing here {{ 'yet' + '!' }}</p>
+  <p>Simple sum: 1 + 2 = {{ 1 + 2 }}</p>
+</template>
+
+<style>
+</style>
