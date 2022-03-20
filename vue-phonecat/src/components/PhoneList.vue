f57bda855fd3f7790a765d48f5dba9ebb48c310a
Antony Nyagah <tony.m.nyagah@gmail.com>
2022-03-20 22:50:43 +0300

    added a searchbox to filter phones by name - this involved adding a v-model to an input field for 2 way binding

--- vue-phonecat/src/components/PhoneList.vue
+++ vue-phonecat/src/components/PhoneList.vue
@@ -5,19 +5,42 @@ export default {
     name: "PhoneList",
     data() {
         return {
-            phones: json
+            phones: json,
+            searchQuery: "",
         }
-    }
+    },
+    computed: {
+        filteredPhones() {
+            let filteredPhones = this.phones.filter(phone => {
+                return phone.name.toLowerCase().includes(this.searchQuery.toLowerCase());
+            });
+            return filteredPhones;
+        }
+    },
 }
+
 </script>
 
 <template>
-    <ul>
-        <li v-for="phone in phones" :key="phone.id">
-            <span>{{ phone.name }}</span>
-            <p>{{ phone.snippet }}</p>
-        </li>
-    </ul>
-
-    <p>Total number of phones: {{ phones.length }}</p>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2">
+                <!-- two way binding with the searchQuery data attribute -->
+                <p>
+                    Search:
+                    <input placeholder="Search phone name" v-model="searchQuery" />
+                </p>
+            </div>
+            <div class="col-md-10">
+                <ul>
+                    <!-- filteredPhones is a function that checks to see if what is in searchQuery is in any of the phone names -->
+                    <li v-for="phone in filteredPhones" :key="phone.id">
+                        <span>{{ phone.name }}</span>
+                        <p>{{ phone.snippet }}</p>
+                    </li>
+                </ul>
+            </div>
+            <p>Total number of phones: {{ filteredPhones.length }}</p>
+        </div>
+    </div>
 </template>
