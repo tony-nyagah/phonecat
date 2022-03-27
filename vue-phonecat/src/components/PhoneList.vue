7de65ebddd9c490341564426036df15224a5d45e
Antony Nyagah <tony.m.nyagah@gmail.com>
2022-03-27 16:00:15 +0300

    added a button for sorting the phone list - added a test to ensure the phone list displays the newest phones on first rendering

--- vue-phonecat/src/components/PhoneList.vue
+++ vue-phonecat/src/components/PhoneList.vue
@@ -7,14 +7,37 @@ export default {
         return {
             phones: json,
             searchQuery: "",
+            orderChoices: ["Name", "Newest"],
+            selectedSortChoice: "Newest",
         }
     },
-    computed: {
+    methods: {
         filteredPhones() {
             let filteredPhones = this.phones.filter(phone => {
                 return phone.name.toLowerCase().includes(this.searchQuery.toLowerCase());
             });
             return filteredPhones;
+        },
+
+        sortedPhones() {
+            let unsortedPhones = this.filteredPhones();
+            if (this.selectedSortChoice === "Name") {
+                unsortedPhones.sort((a, b) => {
+                    return a.name.toLowerCase() > b.name.toLowerCase()
+                })
+            }
+            else if (this.selectedSortChoice === "Newest") {
+                unsortedPhones.sort((a, b) => {
+                    return b.age > a.age;
+                })
+            }
+            let sortedPhones = unsortedPhones;
+            return sortedPhones;
+        }
+    },
+    computed: {
+        filteredAndSortedPhones() {
+            return this.sortedPhones();
         }
     },
 }
@@ -30,17 +53,24 @@ export default {
                     Search:
                     <input placeholder="Search phone name" v-model="searchQuery" />
                 </p>
+
+                <p>
+                    Sort by:
+                    <select v-model="selectedSortChoice">
+                        <option v-for="choice in orderChoices">{{ choice }}</option>
+                    </select>
+                </p>
             </div>
             <div class="col-md-10">
-                <ul>
+                <ul class="phones">
                     <!-- filteredPhones is a function that checks to see if what is in searchQuery is in any of the phone names -->
-                    <li v-for="phone in filteredPhones" :key="phone.id">
+                    <li v-for="phone in filteredAndSortedPhones" :key="phone.id">
                         <span>{{ phone.name }}</span>
                         <p>{{ phone.snippet }}</p>
                     </li>
                 </ul>
             </div>
-            <p>Total number of phones: {{ filteredPhones.length }}</p>
+            <p>Total number of phones: {{ filteredAndSortedPhones.length }}</p>
         </div>
     </div>
 </template>
--- vue-phonecat/src/components/__tests__/PhoneList.spec.js
+++ vue-phonecat/src/components/__tests__/PhoneList.spec.js
@@ -4,9 +4,13 @@ import { mount } from "@vue/test-utils";
 import PhoneList from "../PhoneList.vue";
 
 describe("Phone list", () => {
+  const wrapper = mount(PhoneList);
   it("returns a list of 20 phones", () => {
-    const wrapper = mount(PhoneList);
-    // expect the length of the phone json file tro be 20
+    // expect the length of the phone json file too be 20
     expect(wrapper.vm.phones.length).toBe(20);
+  });
+
+  it("should have the default sort order set to 'newest'", () => {
+    expect(wrapper.vm.selectedSortChoice).toBe("Newest");
   });
 });
